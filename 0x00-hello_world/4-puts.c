098224d98b769093c40d6b3cad0fbc13e87cf181
Sanele <sanele.pholwane28@gmail.com>
2023-02-16 14:53:25 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with a puts function
- * return: 0 (Success)
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0)
+return (0);
 }
